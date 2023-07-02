543084e8e39c8dc17db1ee173db4fca368577146
hoangng26 <hoangnguyenhk14cqt@gmail.com>
2023-07-02 12:17:11 +0700

    Changs: Replace ClientID and ClientSecret in api.config.js

--- config/api.config.js
+++ config/api.config.js
@@ -10,8 +10,8 @@
 module.exports = {
   // The clientId and clientSecret are used to authenticate the user with Microsoft Graph API using OAuth. You would
   // not need to change anything here if you can authenticate with your personal Microsoft account with OneDrive International.
-  clientId: 'd87bcc39-1750-4ca0-ad54-f8d0efbb2735',
-  obfuscatedClientSecret: 'U2FsdGVkX1830zo3/pFDqaBCVBb37iLw3WnBDWGF9GIB2f4apzv0roemp8Y+iIxI3Ih5ecyukqELQEGzZlYiWg==',
+  clientId: '82136479-8be9-4bac-afde-1aec0dcdd79b',
+  obfuscatedClientSecret: 'U2FsdGVkX18gTpHedkUWtvlR32Jn2gjXq7/aoss0p34g2OCs7y/tjXLBwbf88FaH5W0HQwhCMhx5bYoBGFSYQg==',
 
   // The redirectUri is the URL that the user will be redirected to after they have authenticated with Microsoft Graph API.
   // Likewise, you would not need to change redirectUri if you are using your personal Microsoft account with OneDrive International.
@@ -31,4 +31,4 @@ module.exports = {
   // - stale-while-revalidate: allow serving stale content while revalidating on the edge
   // https://vercel.com/docs/concepts/edge-network/caching
   cacheControlHeader: 'max-age=0, s-maxage=60, stale-while-revalidate',
-}
+};
